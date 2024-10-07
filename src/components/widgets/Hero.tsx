60d2ab19379a1b9cbf589018a6af82ae37646438
Parvez Coder <ParvezCoder786@gmail.com>
2024-10-07 22:31:22 +0500

    responsive

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
         {/* left side */}
         <div className='flex-1'>
           <h4 className='text-cyan-500 font-semibold text-lg mt-4 md:mt-0'>Governor initiative for Artifitial  intelligence and Computing (GIAIC)</h4>
-          <h1 className='text-5xl sm:text-6xl font-bold '>Certified Web 3.0 and Metaverse Developer</h1>
+          <h1 className=' text-2xl text-center sm:text-start  sm:text-3xl md:text-4xl lg:text-6xl font-bold '>Certified Web 3.0 and Metaverse Developer</h1>
           <p className='mt-6 text-lg text-slate-800'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe fugit harum at molestias debitis possimus enim.
             Nihil expedita quo omnis magnam </p>
           <div className='mt-6'>
