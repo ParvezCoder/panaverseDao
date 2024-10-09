3f476c6413f3099e75909b93a32b78e1182806be
Parvez Coder <ParvezCoder786@gmail.com>
2024-10-10 04:00:54 +0500

    responsive

--- src/components/widgets/SpecialTracks.tsx
+++ src/components/widgets/SpecialTracks.tsx
@@ -24,19 +24,19 @@ export const programData = [
         ]
     },
     {
-        slug: "wmd",
-        header: "Web 3.0 (Blockchain) and Metaverse Specialization",
-        description: "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences",
-        image: web,
+        slug: "ai",
+        header: "Artificial Intelligence (AI) and Deep Learning Specialization",
+        description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+        image: ai,
         quarters: [
             {
                 header: "Quarter IV",
-                description: "W3- 351: Developing Smart Contracts and Planet - Scale Web 3.0 Dapps",
+                description: "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
                 number: 4
             },
             {
                 header: "Quarter V",
-                description: "MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences",
+                description: "AI-361: Deep Learning and MLOps",
                 number: 5
             }
         ]
@@ -60,19 +60,19 @@ export const programData = [
         ]
     },
     {
-        slug: "wmd",
-        header: "Web 3.0 (Blockchain) and Metaverse Specialization",
-        description: "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences",
-        image: web,
+        slug: "ai",
+        header: "Artificial Intelligence (AI) and Deep Learning Specialization",
+        description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+        image: ai,
         quarters: [
             {
                 header: "Quarter IV",
-                description: "W3- 351: Developing Smart Contracts and Planet - Scale Web 3.0 Dapps",
+                description: "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
                 number: 4
             },
             {
                 header: "Quarter V",
-                description: "MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences",
+                description: "AI-361: Deep Learning and MLOps",
                 number: 5
             }
         ]
@@ -96,19 +96,19 @@ export const programData = [
         ]
     },
     {
-        slug: "wmd",
-        header: "Web 3.0 (Blockchain) and Metaverse Specialization",
-        description: "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences",
-        image: web,
+        slug: "ai",
+        header: "Artificial Intelligence (AI) and Deep Learning Specialization",
+        description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+        image: ai,
         quarters: [
             {
                 header: "Quarter IV",
-                description: "W3- 351: Developing Smart Contracts and Planet - Scale Web 3.0 Dapps",
+                description: "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
                 number: 4
             },
             {
                 header: "Quarter V",
-                description: "MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences",
+                description: "AI-361: Deep Learning and MLOps",
                 number: 5
             }
         ]
@@ -131,20 +131,21 @@ export const programData = [
             }
         ]
     },
+    
     {
-        slug: "wmd",
-        header: "Web 3.0 (Blockchain) and Metaverse Specialization",
-        description: "This Web 3.0 and Metaverse specialization focuses on developing full-stack Web 3.0 and Metaverse experiences for the next generation of the internet by specializing in building worlds that merge the best of cutting-edge decentralized distributed blockchains with 3D metaverse client experiences",
-        image: web,
+        slug: "ai",
+        header: "Artificial Intelligence (AI) and Deep Learning Specialization",
+        description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
+        image: ai,
         quarters: [
             {
                 header: "Quarter IV",
-                description: "W3- 351: Developing Smart Contracts and Planet - Scale Web 3.0 Dapps",
+                description: "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
                 number: 4
             },
             {
                 header: "Quarter V",
-                description: "MV-361: Developing Planet-Scale Open Virtual and Augmented Metaverse Experiences",
+                description: "AI-361: Deep Learning and MLOps",
                 number: 5
             }
         ]
@@ -168,24 +169,6 @@ export const programData = [
         ]
     },
 
-    {
-        slug: "ai",
-        header: "Artificial Intelligence (AI) and Deep Learning Specialization",
-        description: "The AI and Deep Learning specialization focuses on building and deploying intelligent APIs using OpenAI models and building custom Deep Learning Tensorflow models.",
-        image: ai,
-        quarters: [
-            {
-                header: "Quarter IV",
-                description: "AI-351: Developing Planet-Scale Intelligent APIs and Python Programming",
-                number: 4
-            },
-            {
-                header: "Quarter V",
-                description: "AI-361: Deep Learning and MLOps",
-                number: 5
-            }
-        ]
-    }
 
 ]
 const SpecialTracks = () => {
